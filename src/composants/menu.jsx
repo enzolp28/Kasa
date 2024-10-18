3ca4f0d85a30028aa8e332fa903fc49c4f799d0a
enzolp28 <laposta.enzo@gmail.com>
2024-10-18 15:22:54 +0200

    menu avec useRef

--- src/composants/menu.jsx
+++ src/composants/menu.jsx
@@ -1,10 +1,11 @@
 import Arrow from '@/assets/arrow.svg'
 import '../styles/menu.scss'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export default function Menu({ titre }) {
 
     const [open, setOpen] = useState(false)
+    const contentRef = useRef(null);
 
     return (
         <div className={`menu-container ${open ? 'open' : ''}`} >
@@ -12,7 +13,7 @@ export default function Menu({ titre }) {
                 <h3 className='menu__title'>{titre}</h3>
                 <img className="menu__arrow" src={Arrow} alt="flèche menu" />
             </div>
-            <div className="menu-content" >
+            <div className="menu-content" ref={contentRef} style={{ maxHeight: open ? contentRef.current.scrollHeight : 0 }}>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat eveniet aliquam doloremque consectetur. Harum omnis fugit cupiditate repellendus aliquam explicabo.</p>
             </div>
 
--- src/pages/A-propos/Apropos.jsx
+++ src/pages/A-propos/Apropos.jsx
@@ -1,6 +1,4 @@
 import './apropos.scss'
-import Nav from '@/composants/nav.jsx';
-import Footer from '@/composants/footer.jsx';
 import Baniere from '../../composants/baniere';
 import Menu from '../../composants/menu';
 
